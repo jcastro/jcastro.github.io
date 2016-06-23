473f04f6889bf626ea98ee14abd6e201076cde0c
Jonatan Castro <contact@midtonedesign.com>
2016-06-23 14:21:08 +0200

    work update

---
+++ static/scripts/ie-kitten.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function(){
+	$("#messageBox a").mouseover(function(){
+		$("#kitten").animate({
+			backgroundPosition: "0 -243px"
+		},10);
+	});
+	$("#messageBox a").mouseout(function(){
+		$("#kitten").animate({
+			backgroundPosition: "0 0"
+		},10);
+	});
+});
